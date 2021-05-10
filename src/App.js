7c61e2e2fb1efe3b4c3f761f3c0c135e44325ec6
MSLB <msbulkowski@protonmail.com>
2021-05-10 23:58:11 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Calculator from './components/Calculator.js';
+import Footer from './components/Footer.js';
 function App() {
   return (
     <div className="app">
       <Calculator />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+import '../styles/Footer.css';
+export default function Footer() {
+    return (
+        <footer className='footer'>App made by Martin Bulkowski</footer>
+    )
+}
